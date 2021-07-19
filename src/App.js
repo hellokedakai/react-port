5644bf84e275a913ab2e88444870e34034276e6b
Kedakai <nicoleteeth@ucla.edu>
2021-07-18 18:21:58 -0700

    add footer element and change header background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './components/Header'
+import Footer from './components/Footer'
 // import About from './components/About';
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header className = "bg"></Header>
       <main>
         
       </main>
+      <Footer></Footer>
     </div>
   );
 }
