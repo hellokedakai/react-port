ec55d5a09b7ec1243561a027b314841cc7a724db
Kedakai <nicoleteeth@ucla.edu>
2021-07-18 17:35:14 -0700

    add project and portfolio components

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Portfolio() {
+    return(
+        <section>
+            <h1>
+                Portfolio
+            </h1>
+        </section>
+    )
+}
+
+export default Portfolio;
