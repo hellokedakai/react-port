977a00c193e17d0b6d2f230e927d4e0fdd3d56ed
Kedakai <nicoleteeth@ucla.edu>
2021-11-14 19:09:44 -0800

    switch out resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
     <section>
       <h1 id="about">About Me</h1>
       <img src={profileImage} className="my-2" style={{ width: "10%" }} alt="profile photo" />
-      <p>I am an aspiring full stack developer and hobbyist.</p>
+      <p>Full Stack Web Developer with a strong professional background, highly skilled in APIs, MySQL, React, and responsive design.</p>
     </section>
   );
 }
